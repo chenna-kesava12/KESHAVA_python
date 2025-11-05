3a46a3b41aa51ccd98e4a385d5fe13a193d67536
chenna-kesava12 <kesavakesava707@gmail.com>
2025-11-05 15:04:41 +0530

    Time: 11 ms (78.72%), Space: 12.4 MB (82.9%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,27 +1,19 @@
-class Solution(object):
+class Solution:
     def convert(self, s, numRows):
-        """
-        :type s: str
-        :type numRows: int
-        :rtype: str
-        """
-        # If there is only one row or numRows >= length of string, return the string as is
         if numRows == 1 or numRows >= len(s):
             return s
 
-        # Create a list to hold each row's characters
+        # Create a list of strings for each row
         rows = [''] * numRows
         curr_row = 0
         going_down = False
 
-        # Iterate through each character in the string
         for c in s:
             rows[curr_row] += c
-            # Reverse direction if we are at top or bottom row
+            # Change direction at the first or last row
             if curr_row == 0 or curr_row == numRows - 1:
                 going_down = not going_down
-            # Move to next row depending on direction
             curr_row += 1 if going_down else -1
 
-        # Join all rows together
+        # Concatenate all rows
         return ''.join(rows)
