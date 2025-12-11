93d1773ec8dba2c2b101881bfd075e991dfb3b6f
chenna-kesava12 <kesavakesava707@gmail.com>
2025-12-11 14:16:35 +0530

    Time: 0 ms (100%), Space: 12.3 MB (83.63%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,32 +2,31 @@ class Solution:
     def letterCombinations(self, digits):
         if not digits:
             return []
-
-        # Mapping of digits to corresponding letters
+        
+        # Digit to letters mapping
         phone_map = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz"
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
         }
-
+        
         res = []
-
+        
         def backtrack(index, path):
-            # If full combination formed, add to result
+            # If the path length equals digits length, we have a complete combination
             if index == len(digits):
                 res.append("".join(path))
                 return
-
-            # Get letters for current digit
-            for ch in phone_map[digits[index]]:
-                path.append(ch)
+            
+            for char in phone_map[digits[index]]:
+                path.append(char)
                 backtrack(index + 1, path)
-                path.pop()  # backtrack (remove last letter)
-
+                path.pop()  # backtrack
+        
         backtrack(0, [])
         return res
