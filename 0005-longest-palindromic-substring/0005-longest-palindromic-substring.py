44af67714487488879296668c1b37af012bf232d
chenna-kesava12 <kesavakesava707@gmail.com>
2025-12-11 13:53:30 +0530

    Time: 317 ms (92.72%), Space: 12.6 MB (55.93%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,27 +1,20 @@
-def longestPalindrome(s: str) -> str:
-    if not s:
-        return ""
-    
-    start, end = 0, 0
-    
-    for i in range(len(s)):
-        # For odd length palindromes
-        len1 = expand(s, i, i)
-        # For even length palindromes
-        len2 = expand(s, i, i + 1)
-        
-        long_len = max(len1, len2)
-        
-        if long_len > (end - start):
-            start = i - (long_len - 1) // 2
-            end = i + long_len // 2
-    
-    return s[start:end + 1]
+class Solution:
+    def longestPalindrome(self, s):
+        if not s:
+            return ""
+        start, end = 0, 0
+        for i in range(len(s)):
+            len1 = self._expand(s, i, i)      # odd-length
+            len2 = self._expand(s, i, i + 1)  # even-length
+            long_len = len1 if len1 > len2 else len2
+            if long_len > (end - start):
+                start = i - (long_len - 1) // 2
+                end = i + long_len // 2
+        return s[start:end + 1]
 
-
-def expand(s, left, right):
-    while left >= 0 and right < len(s) and s[left] == s[right]:
-        left -= 1
-        right += 1
-    return right - left - 1
+    def _expand(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
         
