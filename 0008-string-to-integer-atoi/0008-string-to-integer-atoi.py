e585b2b6079bf4578240133e4136cfb1e1f1e4ae
chenna-kesava12 <kesavakesava707@gmail.com>
2026-01-23 13:53:43 +0530

    Time: 4 ms (15.44%), Space: 19.6 MB (13.04%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,46 +1,22 @@
 class Solution:
-    def myAtoi(self, s):
-        INT_MIN = -2**31          # -2147483648
-        INT_MAX = 2**31 - 1       #  2147483647
-        
-        i = 0
-        n = len(s)
-        
-        # 1. Skip leading whitespace
-        while i < n and s[i] == ' ':
-            i += 1
-        
-        # If string ended
-        if i >= n:
+    def myAtoi(self, s: str) -> int:
+        s=s.lstrip()
+        if not s:
             return 0
-        
-        # 2. Check for sign
-        sign = 1
-        if s[i] == '-':
-            sign = -1
-            i += 1
-        elif s[i] == '+':
-            i += 1
-        
-        # 3. Convert digits
-        result = 0
-        found_digit = False
-        
-        while i < n and s[i].isdigit():
-            found_digit = True
-            digit = ord(s[i]) - ord('0')
-            i += 1
-            
-            # Check overflow BEFORE adding digit
-            if result > INT_MAX // 10 or (result == INT_MAX // 10 and digit > 7):
-                return INT_MAX if sign == 1 else INT_MIN
-            
-            result = result * 10 + digit
-        
-        if not found_digit:
-            return 0
-        
-        return sign * result
-
-
-        
+        sign=1
+        if s[0] in ["-","+"]:
+            if s[0]=="-":
+                sign=-1
+            s=s[1:]
+        i=0
+        c=0
+        while i<len(s) and s[i].isdigit():
+            c=c*10+int(s[i])
+            i+=1
+        c*=sign
+        min,max=-2**31,2**31-1
+        if c<min:
+            return min
+        if c>max:
+            return max
+        return c     
