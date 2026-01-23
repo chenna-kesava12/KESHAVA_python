b74d97c32cd05d009c396f6c4aad3ad332bdcbdb
chenna-kesava12 <kesavakesava707@gmail.com>
2026-01-23 13:57:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,28 +1,26 @@
 class Solution:
-    def intToRoman(self, num):
-        # Mapping of values to Roman numerals, including subtractive forms
-        val_to_roman = [
-            (1000, 'M'),
-            (900, 'CM'),
-            (500, 'D'),
-            (400, 'CD'),
-            (100, 'C'),
-            (90, 'XC'),
-            (50, 'L'),
-            (40, 'XL'),
-            (10, 'X'),
-            (9, 'IX'),
-            (5, 'V'),
-            (4, 'IV'),
-            (1, 'I')
+    def intToRoman(self, num: int) -> str:
+        roman_map = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
         ]
-        
-        res = ""
-        for value, roman in val_to_roman:
-            # Append as many times as we can subtract
+
+        result = ""
+
+        for value, symbol in roman_map:
             while num >= value:
-                res += roman
+                result += symbol
                 num -= value
-        
-        return res
-        
+
+        return result
